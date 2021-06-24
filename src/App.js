43d969475da49bd40e77dc803e9058ea3bff6218
Robson Inocêncio <robson.inocencio@gmail.com>
2021-06-24 16:01:59 -0300

    Criando componente Video

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import Video from '../src/components/Video'
 import Sidebar from '../src/components/Sidebar'
+
 function App() {
   return (
     <div className="App">
+      <Video />
       <Sidebar />
     </div>
   );
